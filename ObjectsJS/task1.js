02a4d94c7bac7d1de707e498389500d54e982527
Iryna Zahaikevych <izahaikevych@gmail.com>
2026-04-02 21:10:24 +0300

    feat: Completed homework9

---
+++ ObjectsJS/task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firsName = "Iryna";
+student.lastName = "Zahaikevych";
+student.age = 34;
+student.cources = ["Automation testing", "Java Script", "C#"];
+
+console.log(student);
